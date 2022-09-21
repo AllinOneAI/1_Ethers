4f55971a8c1ff64d22e01a9fe937c82399851db2
AllinOne <1999easyoz@gmail.com>
2022-09-22 01:10:21 +0300

    ready for action

--- scr/script.js
+++ scr/script.js
@@ -4,13 +4,12 @@ const input = document.querySelector("#input");
 const check = document.querySelector("#button");
 const reg   = new RegExp(input.pattern);
 
-const provider = ethers.providers.InfuraProvider("goerli", "https://goerli.infura.io/v3/dfbf0e548d9d496bae0e3f6fc91cb0a8");
+const provider = new ethers.providers.InfuraProvider("goerli", "dfbf0e548d9d496bae0e3f6fc91cb0a8");
 
-let balance;
 
 async function checkBalance(){
-    balance = await provider.getBalance(input.value);
-    alert(ethers.utils.formatUnits(balance));
+    const balance = await provider.getBalance(input.value);
+    alert(`\nETH balance of entered address is ${ethers.utils.formatEther(balance)} ETH\n`);
 }
 
 input.oninput = () => {
